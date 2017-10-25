7b5c878e053e4bc8cd8054f5b2e77536b1310b4a
juanmacicao <juanmacicao@gmail.com>
2017-10-25 15:11:28 -0300

    Adds gravatar_url to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :nickname, :email, :tel, :web, :city, :country
+  attributes :id, :name, :nickname, :email, :tel, :web, :city, :country, :gravatar_url
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(object.email).downcase
+    url = "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
