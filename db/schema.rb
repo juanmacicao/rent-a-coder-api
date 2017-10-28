a41d2d4b59399dd340d3fcde142ed9144af53db3
juanmacicao <juanmacicao@gmail.com>
2017-10-28 17:04:00 -0300

    Adds Technology model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027153513) do
+ActiveRecord::Schema.define(version: 20171028195402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20171027153513) do
     t.bigint "developer_id"
     t.index ["developer_id"], name: "index_projects_on_developer_id"
     t.index ["owner_id"], name: "index_projects_on_owner_id"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
