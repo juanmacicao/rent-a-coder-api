2c74e4af6d090304cefd67ef4a9ea660baaa0d83
juanmacicao <juanmacicao@gmail.com>
2017-10-28 10:38:52 -0300

    Validates project's attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   belongs_to :developer, class_name: 'User', foreign_key: :developer_id, optional: true
+
+  validates :name, :description, :deadline, presence: true
+  validate :deadline_not_in_past
+
+  def deadline_not_in_past
+    errors.add(:deadline, "must be in future") if deadline.present? && deadline <= Date.today
+  end
 end
