5d56048a4e77c9d0ba87575a54ae3a38317d2262
juanmacicao <juanmacicao@gmail.com>
2017-11-07 11:01:09 -0300

    Adds technologies to full project serializer

--- app/serializers/full_project_serializer.rb
+++ app/serializers/full_project_serializer.rb
@@ -1,6 +1,8 @@
 class FullProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :deadline, :state, :owner
 
+  has_many :technologies
+
   def owner
     OwnerSerializer.new(object.owner)
   end
