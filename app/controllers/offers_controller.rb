8af8e8244935e7eaf23674e3cf11180ea619d5a0
juanmacicao <juanmacicao@gmail.com>
2017-11-14 19:24:43 -0300

    Updates project state when user creates the first offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,7 @@ class OffersController < ApplicationController
   def create
     offer = Offer.new(offer_params)
     if offer.save
+      update_project_state(offer.project)
       render json: offer, status: 201
     else
       render json: { errors: offer.errors }, status: 422
@@ -13,5 +14,12 @@ class OffersController < ApplicationController
   private
   def offer_params
     params.permit(:cost, :estimated_time, :project_id).merge(user_id: current_user.id)
+  end
+
+  def update_project_state(project)
+    if project.state == 'open'
+      project.state = 'offered'
+      project.save
+    end
   end
 end
